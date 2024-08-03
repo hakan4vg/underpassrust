a0d2feb26708984e2606db685ab9347989e7eb23
Hakan Avgin <hakan4vg@gmail.com>
2024-08-03 18:35:05 +0300

    Initial commit and hello world

---
+++ src/helloworld.rs
@@ -0,0 +1,4 @@
+pub fn helloworld() -> String{
+    let hello = String::from("Hello world!");
+    hello
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod helloworld;
+
+fn main() {
+    let greeting = helloworld::helloworld();
+    println!("{}", greeting);
+}
