a6b818e928c9f5480698e9727f26c02292e97167
Hakan Avgin <hakan4vg@gmail.com>
2024-08-20 00:11:29 +0300

    implement guessnumber, refactor main

--- src/guessnumber.rs
+++ src/guessnumber.rs
@@ -1,4 +1,46 @@
-pub fn guess(){
+use std::io;
+use std::io::Write;
+use rand::Rng;
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+pub fn guess(){
+    let mut count = 0;
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    let number_first = secret_number/10;
+    let number_second = secret_number%10;
+    println!("Guess the number: ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number: ");
+                io::stdout().flush().expect("Failed to flush stdout");
+
+                continue;
+            }
+        };
+        let guess_first = guess/10;
+        let guess_second = guess%10;
+        if guess == secret_number {
+            println!("You guessed the number in {} attempts!", count);
+            io::stdout().flush().expect("Failed to flush stdout");
+
+            break;
+        }
+        else if guess_first == number_first || guess_second == number_second {
+            println!("You are close to the number: ");
+            io::stdout().flush().expect("Failed to flush stdout");
+            count += 1;
+
+        }
+        else {
+            println!("Try again: ");
+            io::stdout().flush().expect("Failed to flush stdout");
+            count += 1;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,57 +13,82 @@ fn main() {
     let greeting = helloworld::helloworld();
     println!("{}", greeting);
 
-
     //Holy shit working with castings and flushes are hard!
-    loop {
-        print!("Enter operation (+, -, *, /, %, ^, !, q to quit): ");
+    loop{
+        print!("Select code snippet: 1-Basic Calculator, 2-Guess Number, 3-Exit: ");
         io::stdout().flush().expect("Failed to flush stdout");
+        let selection_input = read_input();
+        let selection_input: u32 = match selection_input.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                io::stdout().flush().expect("Failed to flush stdout");
 
-
-        let input = read_input();
-        if input.is_empty() {
-            println!("No input provided. Please enter a valid operation.");
-            continue;
-        }
-
-        let operation = input.chars().next().unwrap();
-
-        if operation == 'q' {
-            break;
-        }
-
-        let mut a: i32;
-        let mut b: i32;
-
-        if operation != '!' {
-            print!("Enter first number: ");
-            io::stdout().flush().expect("Failed to flush stdout");
-            a = read_input().parse().expect("Please enter a valid number");
-
-            print!("Enter second number: ");
-            io::stdout().flush().expect("Failed to flush stdout");
-            b = read_input().parse().expect("Please enter a valid number");
-        } else {
-            print!("Enter number: ");
-            io::stdout().flush().expect("Failed to flush stdout");
-            a = read_input().parse().expect("Please enter a valid number");
-            b = 0; // b is not used in factorial
-        }
-
-        let result:f32 = match operation {
-            '+' => basiccalculator::add(a, b) as f32,
-            '-' => basiccalculator::subtract(a, b) as f32,
-            '*' => basiccalculator::multiply(a, b) as f32,
-            '/' => basiccalculator::divide(a, b),
-            '%' => basiccalculator::modulo(a, b) as f32,
-            '^' => basiccalculator::power(a, b) as f32,
-            '!' => basiccalculator::factorial(a) as f32,
-            _ => {
-                println!("Invalid operation");
                 continue;
             }
         };
 
-        println!("Result: {}", result);
+        if selection_input == 1{
+            loop {
+                print!("Enter operation (+, -, *, /, %, ^, !, q to quit): ");
+                io::stdout().flush().expect("Failed to flush stdout");
+
+
+                let input = read_input();
+                if input.is_empty() {
+                    println!("No input provided. Please enter a valid operation.");
+                    continue;
+                }
+
+                let operation = input.chars().next().unwrap();
+
+                if operation == 'q' {
+                    break;
+                }
+
+                let a: i32;
+                let b: i32;
+
+                if operation != '!' {
+                    print!("Enter first number: ");
+                    io::stdout().flush().expect("Failed to flush stdout");
+                    a = read_input().parse().expect("Please enter a valid number");
+
+                    print!("Enter second number: ");
+                    io::stdout().flush().expect("Failed to flush stdout");
+                    b = read_input().parse().expect("Please enter a valid number");
+                } else {
+                    print!("Enter number: ");
+                    io::stdout().flush().expect("Failed to flush stdout");
+                    a = read_input().parse().expect("Please enter a valid number");
+                    b = 0; // b is not used in factorial
+                }
+
+                let result:f32 = match operation {
+                    '+' => basiccalculator::add(a, b) as f32,
+                    '-' => basiccalculator::subtract(a, b) as f32,
+                    '*' => basiccalculator::multiply(a, b) as f32,
+                    '/' => basiccalculator::divide(a, b),
+                    '%' => basiccalculator::modulo(a, b) as f32,
+                    '^' => basiccalculator::power(a, b) as f32,
+                    '!' => basiccalculator::factorial(a) as f32,
+                    _ => {
+                        println!("Invalid operation");
+                        continue;
+                    }
+                };
+
+                println!("Result: {}", result);
+            }
+        }
+        else if selection_input == 2{
+            guessnumber::guess();
+        }
+        else if selection_input == 3{
+            break;
+        }
+        else {
+            println!("Invalid selection. Please enter a valid selection.");
+        }
     }
 }
